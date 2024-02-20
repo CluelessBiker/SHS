636d0eed1c0546b3ca973ad520ff86a6684cb126
Lauren-Nicole <100797990+CluelessBiker@users.noreply.github.com>
2024-02-20 16:12:01 +0200

    fix(i18n.ts): add backend

--- src/i18n.ts
+++ src/i18n.ts
@@ -3,7 +3,6 @@ import { initReactI18next } from 'react-i18next';
 import Backend from 'i18next-http-backend';
 import LanguageDetector from 'i18next-browser-languagedetector';
 import moment from 'moment';
-// import supported locales from moment
 import 'moment/locale/el';
 
 i18n
@@ -17,9 +16,13 @@ i18n
     interpolation: {
       escapeValue: false,
     },
+    backend: {
+      loadPath: '/locales/{{lng}}/{{ns}}.json',
+    },
   })
   .then(() => {
     moment.locale(i18n.language);
+    // initReactI18next(i18n);
   });
 
 i18n.on('languageChanged', lng => {
