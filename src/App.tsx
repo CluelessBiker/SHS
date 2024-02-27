f853582af5d262f7cfbbc4cb49e2a9944f84e13e
Lauren-Nicole <100797990+CluelessBiker@users.noreply.github.com>
2024-02-27 15:15:34 +0200

    fix(App): setup router structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,26 @@
 import './App.css';
 import Navbar from './components/Navbar';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import AboutPage from './pages/AboutPage';
+import LocationsPage from './pages/LocationsPage';
 
 function App() {
   return (
     <div className={'bodyContainer'}>
-      <Navbar />
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path={'/'} element={<HomePage />} />
+          <Route path={'/about'} element={<AboutPage />} />
+          <Route path={'/locations'} element={<LocationsPage />} />
+          <Route path={'/services'} element={<></>} />
+          <Route path={'/practitioners'} element={<></>} />
+          <Route path={'/contact'} element={<></>} />
+          {/* FIXME: replace with 404 page later */}
+          <Route element={<h1>Page not found</h1>} />
+        </Routes>
+      </Router>
     </div>
   );
 }
