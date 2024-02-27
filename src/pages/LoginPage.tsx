8908f8f725bcd58e757a33246d2d1dbe8a9f6a3d
Lauren-Nicole <100797990+CluelessBiker@users.noreply.github.com>
2024-02-27 17:38:32 +0200

    feat(LoginPage): add input fields & button

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,5 +1,60 @@
+import { Box, TextField } from '@mui/material';
+import axios from 'axios';
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
 const LoginPage = () => {
-  return <div>LoginPage</div>;
+  const navigate = useNavigate();
+
+  const [login, setLogin] = useState<{ username: string; password: string }>({
+    username: '',
+    password: '',
+  });
+
+  const onChange = (
+    fieldName: string,
+    event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>,
+  ) => {
+    const value = event?.target.value;
+    setLogin(oldValue => ({
+      ...oldValue,
+      [fieldName]: value,
+    }));
+  };
+
+  const handleLogin = async (event: any) => {
+    event.preventDefault();
+    try {
+      await axios.post('/dj-rest-auth/login/', login);
+      navigate('/');
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <Box>
+      <TextField
+        type="text"
+        label={'username'}
+        variant={'outlined'}
+        id={'outlined-basic'}
+        value={login.username}
+        aria-label={'enter login username'}
+        onChange={value => onChange('username', value)}
+      />
+      <TextField
+        type={'password'}
+        label="password"
+        variant={'outlined'}
+        id={'outlined-basic'}
+        value={login.password}
+        aria-label={'enter login password'}
+        onChange={value => onChange('password', value)}
+      />
+      <button onClick={handleLogin}>login</button>
+    </Box>
+  );
 };
 
 export default LoginPage;
