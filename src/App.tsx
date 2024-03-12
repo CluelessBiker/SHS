88981b9de22d2c8c2fc6bbcb63c5751704383f0d
Lauren-Nicole <100797990+CluelessBiker@users.noreply.github.com>
2024-03-12 15:03:20 +0200

    feat(App.tsx): implement context for logged in user

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import React, { createContext, useEffect, useState } from 'react';
 import './App.css';
 import Navbar from './components/Navbar';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
@@ -6,26 +7,51 @@ import AboutPage from './pages/AboutPage';
 import LocationsPage from './pages/LocationsPage';
 import LoginPage from './pages/LoginPage';
 import './api/axiosDefaults';
+import axios from 'axios';
+
+export const CurrentUserContext = createContext<null | any>(null);
+export const SetCurrentUserContext = createContext<React.Dispatch<
+  React.SetStateAction<any>
+> | null>(null);
 
 function App() {
+  const [currentUser, setCurrentUser] = useState<null>(null);
+
+  const handleMount = async () => {
+    try {
+      const { data } = await axios.get('dj-rest-auth/user/');
+      setCurrentUser(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    handleMount();
+  }, []);
+
   return (
-    <div className={'bodyContainer'}>
-      <Router>
-        <Navbar />
-        <Routes>
-          <Route path={'/'} element={<HomePage />} />
-          <Route path={'/about'} element={<AboutPage />} />
-          <Route path={'/locations'} element={<LocationsPage />} />
-          <Route path={'/services'} element={<></>} />
-          <Route path={'/practitioners'} element={<></>} />
-          <Route path={'/contact'} element={<></>} />
-          <Route path={'/login'} element={<LoginPage />} />
-          {/* FIXME: replace with 404 page later */}
-          <Route element={<h1>Page not found</h1>} />
-        </Routes>
-        <p>eventual footer</p>
-      </Router>
-    </div>
+    <CurrentUserContext.Provider value={currentUser}>
+      <SetCurrentUserContext.Provider value={setCurrentUser}>
+        <div className={'bodyContainer'}>
+          <Router>
+            <Navbar />
+            <Routes>
+              <Route path={'/'} element={<HomePage />} />
+              <Route path={'/about'} element={<AboutPage />} />
+              <Route path={'/locations'} element={<LocationsPage />} />
+              <Route path={'/services'} element={<></>} />
+              <Route path={'/practitioners'} element={<></>} />
+              <Route path={'/contact'} element={<></>} />
+              <Route path={'/login'} element={<LoginPage />} />
+              {/* FIXME: replace with 404 page later */}
+              <Route element={<h1>Page not found</h1>} />
+            </Routes>
+            <p>eventual footer</p>
+          </Router>
+        </div>
+      </SetCurrentUserContext.Provider>
+    </CurrentUserContext.Provider>
   );
 }
 
