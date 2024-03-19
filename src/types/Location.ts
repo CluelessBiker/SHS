ce38fc4d81305723eb3f970f2d68319025abf5da
Lauren-Nicole <100797990+CluelessBiker@users.noreply.github.com>
2024-03-19 08:44:50 +0200

    fix(Location.ts): implement typeset

---
+++ src/types/Location.ts
@@ -0,0 +1,13 @@
+export type Location = {
+  title: string;
+  phone: string;
+  email: string;
+  streetNum: string;
+  street: string;
+  city: string;
+  postcode: string;
+  gRating: string;
+  gMap: string;
+  image: string;
+  language: string;
+};
