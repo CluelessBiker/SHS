8de89b292d5cdecaf190600599ecf7fc3ac28401
Lauren-Nicole <100797990+CluelessBiker@users.noreply.github.com>
2024-03-19 11:30:19 +0200

    feat(ModalBase): implement base modal

---
+++ src/components/ModalBase.tsx
@@ -0,0 +1,29 @@
+import '../App.css';
+import styles from '../styles/ModalBase.module.css';
+import { FC, ReactNode } from 'react';
+import { Box, Modal } from '@mui/material';
+
+type Props = {
+  open: boolean;
+  children: ReactNode;
+  setOpen: (open: boolean) => void;
+};
+
+const ModalBase: FC<Props> = ({ open, setOpen, children }) => {
+  const handleClose = () => setOpen(false);
+
+  return (
+    <div>
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box className={`boxes ${styles.modal}`}>{children}</Box>
+      </Modal>
+    </div>
+  );
+};
+
+export default ModalBase;
