77815f58bd6e41797a3c312ff89425c6b58f4a46
Lauren-Nicole <100797990+CluelessBiker@users.noreply.github.com>
2024-03-19 08:43:37 +0200

    fix(main): import CurrentUserContext

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,15 @@ import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 import './i18n';
+import { CurrentUserProvider } from './context/CurrentUserContext.tsx';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <CurrentUserProvider>
+        <App />
+      </CurrentUserProvider>
+    </Router>
   </React.StrictMode>,
 );
