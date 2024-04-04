26badaef3e350d24e6b9db160beab924a3f12dc8
Lauren-Nicole <100797990+CluelessBiker@users.noreply.github.com>
2024-04-04 11:26:46 +0300

    fix(LocationsPage.tsx): fetch data & display to user

--- src/pages/LocationsPage.tsx
+++ src/pages/LocationsPage.tsx
@@ -1,5 +1,29 @@
+import { useEffect, useState } from 'react';
+import { axiosReq } from '../api/axiosDefaults.ts';
+import LocationData from '../components/LocationData.tsx';
+
 const LocationsPage = () => {
-  return <div>LocationsPage</div>;
+  const [locations, setLocations] = useState<Location[]>([]);
+
+  useEffect(() => {
+    const fetchLocations = async () => {
+      try {
+        const { data } = await axiosReq.get(`/locations/`);
+        setLocations(data.results);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+
+    fetchLocations();
+  }, []);
+
+  return (
+    <div className={'boxInner'}>
+      {locations.length > 0 &&
+        locations.map((data: Location) => <LocationData data={data} />)}
+    </div>
+  );
 };
 
 export default LocationsPage;
