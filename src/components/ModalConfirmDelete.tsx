98235696b7b0554a2af5a0a5b250ec32eb3a776c
Lauren-Nicole <100797990+CluelessBiker@users.noreply.github.com>
2024-04-06 15:54:58 +0300

    feat(ModalConfirmDelete): create delete modal - unstyled

---
+++ src/components/ModalConfirmDelete.tsx
@@ -0,0 +1,27 @@
+import { FC } from 'react';
+import { useTranslation } from 'react-i18next';
+import ModalBase from './ModalBase.tsx';
+
+type Props = {
+  text: string;
+  open: boolean;
+  handleDelete: () => void;
+  setOpen: (value: boolean) => void;
+};
+
+const ModalConfirmDelete: FC<Props> = ({ text, open, setOpen, handleDelete }) => {
+  const { t } = useTranslation();
+
+  return (
+    <ModalBase open={open} setOpen={setOpen}>
+      <p>{t('modals.confirmDelete.sure', { item: text })}</p>
+      <p>{t('modals.confirmDelete.noUndo')}</p>
+      <div>
+        <button onClick={() => setOpen(false)}>{t('buttons.cancel')}</button>
+        <button onClick={handleDelete}>{t('buttons.del')}</button>
+      </div>
+    </ModalBase>
+  );
+};
+
+export default ModalConfirmDelete;
