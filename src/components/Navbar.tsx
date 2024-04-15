5d4747ddbbbdeafb74a9f123fc2a0e417f5c0ac7
Lauren-Nicole <100797990+CluelessBiker@users.noreply.github.com>
2024-04-15 12:36:08 +0300

    fix(Navbar.tsx): remove unnecessary pieces

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -6,17 +6,18 @@ import IconButton from '@mui/material/IconButton';
 import Typography from '@mui/material/Typography';
 import Menu from '@mui/material/Menu';
 import MenuIcon from '@mui/icons-material/Menu';
-import Container from '@mui/material/Container';
 import Button from '@mui/material/Button';
 import Tooltip from '@mui/material/Tooltip';
 import MenuItem from '@mui/material/MenuItem';
-import AdbIcon from '@mui/icons-material/Adb';
 import IconGear from '../assets/svgs/IconGear';
 import { useTranslation } from 'react-i18next';
 import { useNavigate } from 'react-router-dom';
 import { useCurrentUser, useSetCurrentUser } from '../context/CurrentUserContext.tsx';
 import axios from 'axios';
 import ModalLocation from './ModalLocation.tsx';
+import IconSHSLogo from '../assets/svgs/IconSHSLogo.tsx';
+import nav from '../styles/Navbar.module.css';
+import BtnBookNow from './BtnBookNow.tsx';
 
 const pages = [
   { text: 'navbar.about', link: 'about' },
@@ -64,148 +65,126 @@ function Navbar() {
   };
 
   return (
-    <AppBar position="static">
-      <Container>
-        <Toolbar disableGutters>
-          <Typography
-            variant="h6"
-            noWrap
-            component="a"
-            href="#app-bar-with-responsive-menu"
+    <AppBar
+      position="static"
+      style={{
+        padding: 'var(--spacing-1)',
+        backgroundColor: 'var(--mui-palette-primary-pink)',
+      }}
+    >
+      <Toolbar disableGutters style={{ justifyContent: 'space-between' }}>
+        <a className={nav.logo} href={'/'}>
+          <IconSHSLogo
+            width={'50px'}
+            height={'50px'}
+            color={'var(--mui-palette-primary-white'}
+          />
+
+          <h2>System Health Spine</h2>
+        </a>
+
+        <Box sx={{ display: { xs: 'flex', md: 'none' } }}>
+          <IconButton
+            size="large"
+            color="inherit"
+            aria-haspopup="true"
+            onClick={handleOpenNavMenu}
+            aria-controls={'menu-appbar'}
+            aria-label="account of current user"
+          >
+            <MenuIcon />
+          </IconButton>
+          <Menu
+            id="menu-appbar"
+            anchorEl={anchorElNav}
+            anchorOrigin={{
+              vertical: 'bottom',
+              horizontal: 'left',
+            }}
+            keepMounted
+            transformOrigin={{
+              vertical: 'top',
+              horizontal: 'left',
+            }}
+            open={Boolean(anchorElNav)}
+            onClose={() => setAnchorElNav(null)}
             sx={{
-              mr: 2,
-              display: { xs: 'none', md: 'flex' },
-              fontFamily: 'monospace',
-              fontWeight: 700,
-              letterSpacing: '.3rem',
-              color: 'inherit',
-              textDecoration: 'none',
+              display: { xs: 'block', md: 'none' },
             }}
           >
-            System Health Spine
-          </Typography>
+            {pages.map(page => (
+              <MenuItem key={page.text} onClick={() => handlePageNavigation(page.link)}>
+                <a>{t(page.text)}</a>
+              </MenuItem>
+            ))}
+          </Menu>
+        </Box>
 
-          <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
-            <IconButton
-              size="large"
-              aria-label="account of current user"
-              aria-controls="menu-appbar"
-              aria-haspopup="true"
-              onClick={handleOpenNavMenu}
-              color="inherit"
+        {/* -----WEB VIEW----- */}
+        <Box sx={{ display: { xs: 'none', md: 'flex' }, alignItems: 'center' }}>
+          {pages.map(page => (
+            <Button
+              key={page.text}
+              onClick={() => handlePageNavigation(page.link)}
+              sx={{
+                my: 2,
+                color: 'white',
+                display: 'block',
+                textTransform: 'capitalize',
+              }}
             >
-              <MenuIcon />
-            </IconButton>
+              {t(page.text)}
+            </Button>
+          ))}
+          <BtnBookNow />
+        </Box>
+
+        {currentUser && (
+          <Box sx={{ flexGrow: 0 }}>
+            <Tooltip title="Open settings">
+              <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+                <IconGear color={'#F1F1F1'} />
+              </IconButton>
+            </Tooltip>
+
             <Menu
+              sx={{ mt: '45px' }}
               id="menu-appbar"
-              anchorEl={anchorElNav}
+              anchorEl={anchorElUser}
               anchorOrigin={{
-                vertical: 'bottom',
-                horizontal: 'left',
+                vertical: 'top',
+                horizontal: 'right',
               }}
               keepMounted
               transformOrigin={{
                 vertical: 'top',
-                horizontal: 'left',
+                horizontal: 'right',
               }}
-              open={Boolean(anchorElNav)}
-              onClose={() => setAnchorElNav(null)}
-              sx={{
-                display: { xs: 'block', md: 'none' },
-              }}
+              open={Boolean(anchorElUser)}
+              onClose={handleCloseUserMenu}
             >
-              {pages.map(page => (
-                <MenuItem key={page.text} onClick={() => handlePageNavigation(page.link)}>
-                  <Typography textAlign="center">{page.text}</Typography>
+              {settings.map((it, ind) => (
+                <MenuItem
+                  key={it.link + ind}
+                  onClick={() =>
+                    it.link === 'logout' ? handleLogout() : handlePageNavigation(it.link)
+                  }
+                >
+                  <Typography textAlign="center">{t(it.text)}</Typography>
                 </MenuItem>
               ))}
+              <MenuItem
+                onClick={() => {
+                  setLocationOpen(true);
+                  setAnchorElNav(null);
+                }}
+              >
+                <Typography textAlign="center">{t('navbar.addLoc')}</Typography>
+              </MenuItem>
             </Menu>
           </Box>
-          <AdbIcon sx={{ display: { xs: 'flex', md: 'none' }, mr: 1 }} />
-          <Typography
-            variant="h5"
-            noWrap
-            component="a"
-            href="#app-bar-with-responsive-menu"
-            sx={{
-              mr: 2,
-              display: { xs: 'flex', md: 'none' },
-              flexGrow: 1,
-              fontFamily: 'monospace',
-              fontWeight: 700,
-              letterSpacing: '.3rem',
-              color: 'inherit',
-              textDecoration: 'none',
-            }}
-          >
-            LOGO
-          </Typography>
-          <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
-            {pages.map(page => (
-              <Button
-                key={page.text}
-                onClick={() => handlePageNavigation(page.link)}
-                sx={{
-                  my: 2,
-                  color: 'white',
-                  display: 'block',
-                  textTransform: 'capitalize',
-                }}
-              >
-                {t(page.text)}
-              </Button>
-            ))}
-          </Box>
-
-          {currentUser && (
-            <Box sx={{ flexGrow: 0 }}>
-              <Tooltip title="Open settings">
-                <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                  <IconGear color={'#F1F1F1'} />
-                </IconButton>
-              </Tooltip>
-
-              <Menu
-                sx={{ mt: '45px' }}
-                id="menu-appbar"
-                anchorEl={anchorElUser}
-                anchorOrigin={{
-                  vertical: 'top',
-                  horizontal: 'right',
-                }}
-                keepMounted
-                transformOrigin={{
-                  vertical: 'top',
-                  horizontal: 'right',
-                }}
-                open={Boolean(anchorElUser)}
-                onClose={handleCloseUserMenu}
-              >
-                {settings.map((it, ind) => (
-                  <MenuItem
-                    key={it.link + ind}
-                    onClick={() =>
-                      it.link === 'logout'
-                        ? handleLogout()
-                        : handlePageNavigation(it.link)
-                    }
-                  >
-                    <Typography textAlign="center">{t(it.text)}</Typography>
-                  </MenuItem>
-                ))}
-                <MenuItem
-                  onClick={() => {
-                    setLocationOpen(true);
-                    setAnchorElNav(null);
-                  }}
-                >
-                  <Typography textAlign="center">{t('navbar.addLoc')}</Typography>
-                </MenuItem>
-              </Menu>
-            </Box>
-          )}
-        </Toolbar>
-      </Container>
+        )}
+      </Toolbar>
       <ModalLocation open={locationOpen} setOpen={setLocationOpen} />
     </AppBar>
   );
