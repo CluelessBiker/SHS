ba804fbf9c6a17424d36917f83ea397fa8eb488b
Lauren-Nicole <100797990+CluelessBiker@users.noreply.github.com>
2024-04-16 12:15:42 +0300

    fix(main.tsx): remove redundant import link

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
-import './index.css';
 import './i18n';
 import { CurrentUserProvider } from './context/CurrentUserContext.tsx';
 import { BrowserRouter as Router } from 'react-router-dom';
