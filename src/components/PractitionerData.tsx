42fdbf4e79c5241c06c3ed969830ee54f1c5e849
Lauren-Nicole <100797990+CluelessBiker@users.noreply.github.com>
2024-04-16 13:32:38 +0300

    fix(PractitionerData.tsx): format & style page

--- src/components/PractitionerData.tsx
+++ src/components/PractitionerData.tsx
@@ -7,25 +7,31 @@ type Props = {
 
 const PractitionerData: FC<Props> = ({ data }) => {
   return (
-    <div>
-      <h3>{data.title}</h3>
-      <img
-        src={data.image}
-        alt={`${data.title} image`}
-        aria-label={`${data.title} image`}
-      />
-      <p>{data.personalExperience}</p>
-      <p>{data.bio}</p>
-      <p>
-        {data.fName} {data.lName}
-      </p>
-      {data.locations.length > 0 &&
-        data.locations.map(it => (
-          <p>
-            {it.title}, {it.city}
-          </p>
-        ))}
-      {data.services.length > 0 && data.services.map(it => <p>{it.title}</p>)}
+    <div className={'boxContent boxVerticalGap'}>
+      <div className={'boxVerticalGap'}>
+        <p>
+          {data.fName} {data.lName}
+        </p>
+        <h3>{data.title}</h3>
+        <img
+          src={data.image}
+          className={'roundImage'}
+          alt={`${data.title} image`}
+          aria-label={`${data.title} image`}
+        />
+      </div>
+      <div className={'boxVerticalGap'}>
+        <p>{data.personalExperience}</p>
+        <p>{data.bio}</p>
+
+        {data.locations.length > 0 &&
+          data.locations.map(it => (
+            <p>
+              {it.title}, {it.city}
+            </p>
+          ))}
+        {data.services.length > 0 && data.services.map(it => <p>{it.title}</p>)}
+      </div>
     </div>
   );
 };
