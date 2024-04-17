aab7f57f4784dcb8422c47185ece89f0867f5093
Lauren-Nicole <100797990+CluelessBiker@users.noreply.github.com>
2024-04-17 15:46:13 +0300

    fix(ServiceData.tsx): format data

--- src/components/ServiceData.tsx
+++ src/components/ServiceData.tsx
@@ -1,5 +1,6 @@
 import { Service } from '../types/Service.ts';
 import { FC } from 'react';
+import { Location } from '../types/Location.ts';
 
 type Props = {
   data: Service;
@@ -7,18 +8,38 @@ type Props = {
 
 const ServiceData: FC<Props> = ({ data }) => {
   return (
-    <div>
-      <h3>{data.title}</h3>
-      <img
-        src={data.image}
-        alt={`${data.title} image`}
-        aria-label={`${data.title} image`}
-      />
-      <p>{data.blurb}</p>
-      <p>{data.description}</p>
-      <p>duration: {data.duration}</p>
-      <p>€{data.price}</p>
-      <p>Offered at: {data.blurb}</p>
+    <div className={'boxContent boxVerticalGap'}>
+      <div className={'boxVerticalGap'}>
+        <h3 className={'contentTitle'}>{data.title}</h3>
+        <img
+          src={data.image}
+          className={'roundImage'}
+          alt={`${data.title} image`}
+          aria-label={`${data.title} image`}
+        />
+      </div>
+      <div className={'boxVerticalGap'}>
+        <p>{data.blurb}</p>
+        <p>{data.description}</p>
+      </div>
+      <div className={'boxVerticalGap'}>
+        {data.duration && (
+          <p>
+            <span className={'dataLabel'}>duration:</span> {data.duration}
+          </p>
+        )}
+        {data.price && (
+          <p>
+            <span className={'dataLabel'}>Price:</span> €{data.price}
+          </p>
+        )}
+        <div className={'boxHorizontalGap'}>
+          <p className={'dataLabel'}>Offered at:</p>
+          {data.locations.map((it: Location) => (
+            <p key={it.id}>{it.title}. </p>
+          ))}
+        </div>
+      </div>
     </div>
   );
 };
