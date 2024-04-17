2a0702acb0bceaaee303806b45f826eb2e413745
Lauren-Nicole <100797990+CluelessBiker@users.noreply.github.com>
2024-04-17 15:45:38 +0300

    fix(LocationData.tsx): remove placeholder data

--- src/components/LocationData.tsx
+++ src/components/LocationData.tsx
@@ -42,11 +42,8 @@ const LocationData: FC<Props> = ({ data, handleEdit, handleDelete }) => {
 
       <div className={'boxVerticalGap'}>
         <iframe
-          src={
-            'https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d12576.444636695656!2d23.7785369!3d37.9978676!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x14a198d495e45169%3A0xbc70c1948b911e1b!2sSystem%20Health%20%26%20Spine!5e0!3m2!1sen!2sgr!4v1713256227369!5m2!1sen!2sgr'
-          }
           loading="lazy"
-          // src={data.gMap}
+          src={data.gMap}
           referrerPolicy="no-referrer-when-downgrade"
         ></iframe>
         <p className={'dataLabel'}>{data.description}</p>
