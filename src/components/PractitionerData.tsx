f74aed82354138c950a88b60d6f98f74ad1fd8da
Lauren-Nicole <100797990+CluelessBiker@users.noreply.github.com>
2024-04-19 13:05:41 +0300

    fix(PractitionerData.tsx): add formatting

--- src/components/PractitionerData.tsx
+++ src/components/PractitionerData.tsx
@@ -1,18 +1,23 @@
 import { FC } from 'react';
 import { Practitioner } from '../types/Practitioner.ts';
+import { useTranslation } from 'react-i18next';
 
 type Props = {
   data: Practitioner;
 };
 
 const PractitionerData: FC<Props> = ({ data }) => {
+  const { t } = useTranslation();
+
   return (
     <div className={'boxContent boxVerticalGap'}>
       <div className={'boxVerticalGap'}>
-        <p>
+        <h3 className={'contentTitle'}>
+          {data.prefix && data.prefix + ' '}
           {data.fName} {data.lName}
-        </p>
-        <h3>{data.title}</h3>
+          {data.degree && ' ' + data.degree}
+        </h3>
+        <h4 className={'subTitle'}>{data.title}</h4>
         <img
           src={data.image}
           className={'roundImage'}
@@ -21,16 +26,22 @@ const PractitionerData: FC<Props> = ({ data }) => {
         />
       </div>
       <div className={'boxVerticalGap'}>
-        <p>{data.personalExperience}</p>
-        <p>{data.bio}</p>
+        <p className={'paragraphs'}>
+          <pre style={{ textWrap: 'wrap' }}>{data.bio}</pre>
+        </p>
+        <h4 className={'subTitle'}>{t('about.personal')}</h4>
+        <p className={'paragraphs'}>
+          <pre style={{ textWrap: 'wrap' }}>{data.personalExperience}</pre>
+        </p>
 
-        {data.locations.length > 0 &&
-          data.locations.map(it => (
-            <p>
-              {it.title}, {it.city}
-            </p>
-          ))}
-        {data.services.length > 0 && data.services.map(it => <p>{it.title}</p>)}
+        <p>
+          <span className={'dataLabel paragraphs'}>{t('about.available')}:</span>
+          {data.locations.length > 0 && data.locations.map(it => <> {it.city}.</>)}
+        </p>
+        <p style={{ textAlign: 'center' }}>
+          <span className={'dataLabel'}>{t('about.provides')}:</span>
+          {data.services.length > 0 && data.services.map(it => <> {it.title}.</>)}
+        </p>
       </div>
     </div>
   );
