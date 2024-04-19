03d4791b8d0e1558e129887bd33e02888588bc61
Lauren-Nicole <100797990+CluelessBiker@users.noreply.github.com>
2024-04-19 13:07:08 +0300

    fix(practitioners.ts): format data in prep for translation

---
+++ src/constants/practitioners.ts
@@ -0,0 +1,83 @@
+const practitionersEN = [
+  {
+    locations: ['athens'],
+    services: [''],
+    prefix: 'Dr.',
+    fName: 'Konstantinos',
+    lName: 'Arhontoudis',
+    degree: 'DC',
+    title: 'Chiropractor',
+    bio:
+      'Dr. Konstantinos (Dino) Arhontoudis, D.C., (Doctor of Chiropractic) was born in Toronto, Canada.  He graduated with his Doctor of Chiropractic (DC) from Cleveland University - Kansas City, Mo. (USA) in 2004.  Shortly after that, he came to Athens, Greece to work with the 2004 Olympic Games to provide care and consultations for many of the Olympic athletes.\n' +
+      'Dr. Dino has close to 20 years of clinical experience and has developed several multi-disciplinary wellness & rehabilitation clinics in Greece, UK, and Canada during his career. \n' +
+      'His international experience has made him one of the most dignified and well sought out chiropractic physicians in his field.  He is a member of the General Chiropractic Council (GCC UK), The Royal College of Chiropractors (UK), The International Federation of Sports Chiropractic (FICS), The Hellenic Chiropractic Association (HCA) as well as a being a certified Medical, Sports Medical and Clinical Acupuncturist with an additional five years training as a Kinesiologist and Physical Educator.  He is also a fully certified Rock Doc through Rocktape in Kinesiotaping, IASTM, and Roc Pod therapy. \n' +
+      'Dr. Dino has worked with several athletes and teams both in the UK and Greece at both amateur, professional and Olympic level.  He resides in Athens, Greece along with his wife and children and maintains a private practice in both Athens, the island of Tinos in the Aegean Cyclades and in central London.',
+    image: '',
+    personalExperience:
+      'Multiple private practices in Athens, Greece and London, UK\n' +
+      'GCC (UK), ECU (Europe) and HCA (Greece) registered\n' +
+      'Chiropractor for Athens 2004 Olympics\n' +
+      'Rock Doc Certified (UK)\n' +
+      'Current personal chiropractic physician for multiple professional individual athletes and teams.\n' +
+      'Certified Medical and Traditional Acupuncturist (Canada)\n' +
+      'Primary chiropractor for Greek National Rugby and Ice Hockey Teams as well as individual Olympic athletes\n' +
+      'Lecturer at the European Institute of Integrative Health Sciences (EIIHS)',
+    language: 'en',
+  },
+  {
+    locations: ['athens'],
+    services: [''],
+    prefix: '',
+    fName: 'Benjamin',
+    lName: 'Saez',
+    degree: 'L.Ac',
+    title: 'Acupuncturist & Massage Therapist',
+    bio:
+      'Benjamin Saez is a graduate of International College of Oriental Medicine (ICOM) East Grinstead, UK where he received his Bachelors of Acupuncture in conjunction with Greenwich University.  Prior to graduating with his degree in Oriental Medicine, he studied Ayurveda in Coimbatore, India. \n' +
+      'Shortly after graduating, Benjamin volunteered and provided Acupuncture treatments at the main hospital in Stone Town, Zanzibar working with a group of commissioned Chinese doctors that were lending their aid with the majority of compromised healthcare conditions commonly seen. \n' +
+      'He spent time working with the indigenous population treating hemiplegia, recurrent miscarriage, diabetes, malaria, and a host of other medical conditions inside the hospital setting.  He currently resides in Athens, Greece and maintains a private practice in addition to teaching specialty courses in classical Chinese medicine.',
+    image: '',
+    personalExperience:
+      'Massage Therapist, Arya Vaidya Pharmacy, Coimbatore, India\n' +
+      'Treating physical and psychological conditions in the local Ayurvedic hospital.\n' +
+      'Private Practice, Bordeaux, France\n' +
+      'Provides services in East Asian Medicine which include Acupuncture, Manual Therapy, and Functional Nutrition.\n' +
+      'Acupuncture Physician, Mnazi Moja Hospital, Stone Town, Zanzibar\n' +
+      'Volunteered and provided Acupuncture treatments to the indigenous population of Zanzibar in the main hospital in Stone Town. Worked with commissioned Chinese doctors threatening cases of severe cases of diabetes, hemiplegia, and malaria, along with other medical conditions.\n' +
+      'Private Practice, Athens Acupuncture, Athens, Greece\n' +
+      'Founder, Lecturer & Clinic Supervisor at the European Institute of Integrative Health Sciences (EIIHS).\n' +
+      'Head of Education for the European Traditional Chinese Medicine Association (ETCMA)',
+    language: 'en',
+  },
+  {
+    locations: ['athens'],
+    services: [''],
+    prefix: '',
+    fName: 'Georgette',
+    lName: 'Young',
+    degree: 'PhD',
+    title: 'Acupuncturist',
+    bio:
+      'Georgette Young, L.Ac., DAOM, is a graduate of Pacific College of Oriental Medicine (San Diego) where she received both her Master of Science & Doctoral Degrees in Acupuncture & Oriental Medicine.  She is also California State Licensed & National Board Certified by the National Council Commission for Acupuncture & Oriental Medicine (NCCAOM). These certifications are a mark of excellence that Georgette has met all the professional standards for the safe and competent practice of acupuncture & Chinese herbology.\n' +
+      'As an American living in Athens, Greece, she brings her knowledge and experience in the field of East Asian Medicine & Integrative Women’s Health Care as the first American licensed acupuncturist to be a collaborative staff member at the Institute of Life – a fertility clinic located inside IASO, one of Europe’s largest maternity hospitals.\n' +
+      'In addition, she is an approved Professional Development Educator with the NCCAOM (USA) and is an international consultant and lectures globally on various topics of Traditional East Asian Medicine and Acupuncture for MD’s, Physiotherapists, Psychologists, Nurses, and TCM students.  She also serves as the Hon. Secretary for the European Traditional Chinese Medicine Association (ETCMA) and is involved with the ongoing mission of promoting Chinese medicine in Europe.',
+    image: '',
+    personalExperience:
+      'Private Practice, San Diego, California\n' +
+      'Qualified Medical Evaluator (QME) for the California Worker’s Compensation Division.  Evaluated work related injuries as a neutral 3rd party investigator and provided expert testimonial witness reports for injury disputes.\n' +
+      'Certified in Acupuncture Orthopedics\n' +
+      'Acupuncture Physician at Sea, Steiner Transocean, Miami, Florida\n' +
+      'Pioneered the original Acupuncture-At-Sea Program On-Board Celebrity, Seaborn, Azamara, and Crystal Cruise Ships\n' +
+      'On-Board in-Resident Acupuncture Physician & Oriental Medicine Clinic Manager\n' +
+      'Set up, managed, and directed all aspects of on-board clinic operations: marketing & education, enrichment lectures, and patient care.\n' +
+      'NCCAOM Approved Professional Development Educator, USA & Abroad\n' +
+      'International Professional Development Educator and Consultant Promoting Knowledge of Traditional East Asian Medicine and Acupuncture for MD’s, Physiotherapists, Psychologists, Nurses, and TCM students.\n' +
+      'Assisted Reproductive Acupuncturist, Institute of Life – Athens, Greece\n' +
+      'She brings to Institute of Life her field specialty in TCM Gynecology & Obstetrics and treats a wide variety of conditions related to reproductive and general women’s health\n' +
+      'Honorary Secretary for the European Traditional Chinese Medicine Association (ETCMA)\n' +
+      'Founder, Lecturer & Clinic Supervisor at the European Institute of Integrative Health Sciences (EIIHS).',
+    language: 'en',
+  },
+];
+
+export { practitionersEN };
