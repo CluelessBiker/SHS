d3dcc538b32b023b03e890585bb24b85657538d1
Lauren-Nicole <100797990+CluelessBiker@users.noreply.github.com>
2024-04-26 08:04:56 +0300

    fix(App.tsx): remove .tsx in import statements

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.tsx';
+import App from './App';
 import './i18n';
-import { CurrentUserProvider } from './context/CurrentUserContext.tsx';
+import { CurrentUserProvider } from './context/CurrentUserContext';
 import { BrowserRouter as Router } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
