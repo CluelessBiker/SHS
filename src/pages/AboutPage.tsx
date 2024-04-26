a6a6f861ccaccdcbd2fc69dcff1d38fda3b70c82
Lauren-Nicole <100797990+CluelessBiker@users.noreply.github.com>
2024-04-26 08:04:17 +0300

    style(AboutPage.tsx): reorganise order of html elements

--- src/pages/AboutPage.tsx
+++ src/pages/AboutPage.tsx
@@ -1,5 +1,5 @@
 import abt from '../styles/AboutPage.module.css';
-import BtnBookNow from '../components/BtnBookNow.tsx';
+import BtnBookNow from '../components/BtnBookNow';
 import { useTranslation } from 'react-i18next';
 
 const AboutPage = () => {
@@ -16,6 +16,16 @@ const AboutPage = () => {
         <BtnBookNow />
       </div>
 
+      {/*ELFSIGHT : GOOGLE REVIEWS*/}
+      <div className={'boxImageText'}>
+        <div
+          data-elfsight-app-lazy
+          className="elfsight-app-8476a6be-7dab-46db-91e0-1d5e544f7c4d"
+        />
+      </div>
+
+      <div className={`${abt.midImg} boxImageText`} />
+
       {/*OUR STORY*/}
       <div className={'boxImageText'}>
         <div className={'horizontalRule'} />
@@ -24,16 +34,6 @@ const AboutPage = () => {
           <p>{t('about.ourStryTxt')}</p>
         </div>
         <div className={'horizontalRule'} />
-      </div>
-
-      <div className={`${abt.midImg} boxImageText`} />
-
-      {/*ELFSIGHT : GOOGLE REVIEWS*/}
-      <div className={'boxImageText'}>
-        <div
-          data-elfsight-app-lazy
-          className="elfsight-app-8476a6be-7dab-46db-91e0-1d5e544f7c4d"
-        />
       </div>
     </>
   );
