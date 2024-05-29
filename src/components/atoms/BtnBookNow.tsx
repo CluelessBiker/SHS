407d39587d2a40d0f1345bd4704ba1298fd88d9b
Lauren-Nicole <100797990+CluelessBiker@users.noreply.github.com>
2024-05-29 09:12:32 +0300

    style(BtnBookNow.tsx): convert to styled component

---
+++ src/components/atoms/BtnBookNow.tsx
@@ -0,0 +1,29 @@
+import { useTranslation } from 'react-i18next';
+import styled from 'styled-components';
+
+const StyledButton = styled.a`
+  text-transform: uppercase;
+  border-radius: var(--spacing-half);
+  font-family: var(--typography-face);
+  color: var(--mui-palette-primary-white);
+  font-size: var(--typography-size-caption);
+  font-weight: var(--typography-weight-bold);
+  padding: var(--spacing-1) var(--spacing-2);
+  background-color: var(--mui-palette-primary-green);
+`;
+
+const BtnBookNow = () => {
+  const { t } = useTranslation();
+  return (
+    <StyledButton
+      target={'_blank'}
+      rel={'noreferrer'}
+      aria-label={'book appointment'}
+      href={''}
+    >
+      {t('buttons.bookNow')}
+    </StyledButton>
+  );
+};
+
+export default BtnBookNow;
