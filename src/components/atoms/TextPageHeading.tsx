5ecfddfd066863f2e445b308c89d75b2a2e8d460
Lauren-Nicole <100797990+CluelessBiker@users.noreply.github.com>
2024-05-29 12:43:16 +0300

    feat(TextPageHeading.tsx): implement element & rules as styled component

---
+++ src/components/atoms/TextPageHeading.tsx
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+const TextPageHeading = styled.h1`
+  text-transform: uppercase;
+  font-size: var(--typography-size-title-exLg);
+  font-weight: var(--typography-weight-ultra-light);
+`;
+
+export default TextPageHeading;
