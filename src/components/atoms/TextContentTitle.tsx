761ffa516c5a081ffca21b15877d03473131f060
Lauren-Nicole <100797990+CluelessBiker@users.noreply.github.com>
2024-05-29 12:43:10 +0300

    feat(TextContentTitle.tsx): implement element & rules as styled component

---
+++ src/components/atoms/TextContentTitle.tsx
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+const TextContentTitle = styled.h3`
+  text-align: center;
+  text-transform: uppercase;
+  font-size: var(--typography-size-title-lg);
+`;
+
+export default TextContentTitle;
