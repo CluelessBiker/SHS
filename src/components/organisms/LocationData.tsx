761b27955f6222927e48091acc1e57a4f02ce8b8
Lauren-Nicole <100797990+CluelessBiker@users.noreply.github.com>
2024-05-30 08:11:38 +0300

    fix(LocationData): add optional function prop

--- src/components/organisms/LocationData.tsx
+++ src/components/organisms/LocationData.tsx
@@ -1,82 +1,53 @@
 import { FC } from 'react';
 import { Location } from '../../types/Location';
 import { useCurrentUser } from '../../context/CurrentUserContext';
-import IconEdit from '../../assets/svgs/IconEdit';
-import IconDelete from '../../assets/svgs/IconDelete';
-import btn from '../../styles/Buttons.module.css';
 import { useTranslation } from 'react-i18next';
 import ImageCont from '../atoms/ImageCont';
 import TextContentTitle from '../atoms/TextContentTitle';
+import btn from '../../styles/Buttons.module.css';
+import IconEdit from '../../assets/svgs/IconEdit';
+import IconDelete from '../../assets/svgs/IconDelete';
+import TextKeyValue from '../atoms/TextKeyValue';
+import BoxCard from '../atoms/BoxCard';
 
 type Props = {
   data: Location;
   handleEdit: () => void;
   handleDelete: () => void;
+  handleViewDetails: () => void;
 };
 
-const LocationData: FC<Props> = ({ data, handleEdit, handleDelete }) => {
-  const currentUser = useCurrentUser();
+const LocationData: FC<Props> = ({
+  data,
+  handleEdit,
+  handleDelete,
+  handleViewDetails,
+}) => {
   const { t } = useTranslation();
+  const currentUser = useCurrentUser();
 
   return (
-    <div className={'boxContent boxVerticalGap'}>
-      <div className={'boxVerticalGap'}>
-        <TextContentTitle>{data.title}</TextContentTitle>
-        <ImageCont
-          src={data.image}
-          variant={'square'}
-          altText={`${data.title} location`}
-        />
-      </div>
-      <div className={'boxVerticalGap'}>
-        <p>
-          <span className={'dataLabel'}>{t('contact.form.phn')}:</span> +30 {data.phone}
-        </p>
-        <p>
-          <span className={'dataLabel'}>{t('contact.form.eml')}:</span> {data.email}
-        </p>
+    <BoxCard>
+      <TextContentTitle>{data.title}</TextContentTitle>
+      <ImageCont
+        src={data.image}
+        variant={'square'}
+        onClick={handleViewDetails}
+        altText={`${data.title} location`}
+      />
+      <TextKeyValue keyLeft={t('contact.form.phn')} value={`+30 ${data.phone}`} />
 
-        <br />
-        <p className={'dataLabel'}>{t('contact.form.adr')}:</p>
-        <p>
-          {data.streetNum}, {data.street}
-        </p>
-        <p>
-          {data.area}, {data.city}, {data.postcode}
-        </p>
-      </div>
-
-      <div className={'boxVerticalGap'}>
-        <iframe
-          loading="lazy"
-          src={data.gMap}
-          referrerPolicy="no-referrer-when-downgrade"
-        ></iframe>
-        <p className={'dataLabel'}>{data.description}</p>
-
-        <p>
-          Leave a{' '}
-          <a
-            target={'_blank'}
-            rel={'noreferrer'}
-            href={data.gRating}
-            aria-label={'google reviews'}
-          >
-            review
-          </a>
-        </p>
-        {currentUser && (
-          <div>
-            <button className={btn.iconButton} onClick={handleEdit}>
-              <IconEdit height={'14px'} />
-            </button>
-            <button className={btn.iconButton} onClick={handleDelete}>
-              <IconDelete height={'14px'} />
-            </button>
-          </div>
-        )}
-      </div>
-    </div>
+      {currentUser && (
+        <div>
+          <button className={btn.iconButton} onClick={handleEdit}>
+            <IconEdit height={'14px'} />
+          </button>
+          <button className={btn.iconButton} onClick={handleDelete}>
+            <IconDelete height={'14px'} />
+          </button>
+        </div>
+      )}
+    </BoxCard>
   );
 };
 
