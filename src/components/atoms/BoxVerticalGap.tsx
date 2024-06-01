5cbad6d02fbb351d0a0958cc5668b7afb24dc8f5
Lauren-Nicole <100797990+CluelessBiker@users.noreply.github.com>
2024-06-01 09:33:32 +0300

    fix(BoxVerticalGap.tsx): implement as styled component

---
+++ src/components/atoms/BoxVerticalGap.tsx
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+const BoxVerticalGap = styled.div`
+  display: flex;
+  text-align: center;
+  align-items: center;
+  gap: var(--spacing-2);
+  flex-direction: column;
+`;
+
+export default BoxVerticalGap;
