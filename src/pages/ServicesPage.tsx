68974b391e7a6dfa166c3f1a460c66cf745adf6e
Lauren-Nicole <100797990+CluelessBiker@users.noreply.github.com>
2024-06-01 10:31:52 +0300

    fix(ServicesPage.tsx): import handleLang

--- src/pages/ServicesPage.tsx
+++ src/pages/ServicesPage.tsx
@@ -4,11 +4,15 @@ import { useEffect, useState } from 'react';
 import { axiosReq } from '../api/axiosDefaults';
 import ServiceData from '../components/ServiceData';
 import TextPageHeading from '../components/atoms/TextPageHeading';
+import { handleLang } from '../utils/handleLang';
 
 const ServicesPage = () => {
   const { t } = useTranslation();
+  const lang: 'en' | 'el' | 'fr' =
+    (localStorage.getItem('i18nextLng') as 'en' | 'el' | 'fr') || 'en';
 
   const [services, setServices] = useState<Service[]>([]);
+  const servicesLang = handleLang<Service>(services, lang);
 
   useEffect(() => {
     const fetchServices = async () => {
@@ -27,8 +31,8 @@ const ServicesPage = () => {
     <div className={'boxVerticalGap'} style={{ padding: 'var(--spacing-2)' }}>
       <TextPageHeading>{t('services.offered')}</TextPageHeading>
       <div className={'boxContentContainer'}>
-        {services &&
-          services.map((data: Service) => <ServiceData data={data} key={data.id} />)}
+        {servicesLang &&
+          servicesLang.map((data: Service) => <ServiceData data={data} key={data.id} />)}
       </div>
     </div>
   );
